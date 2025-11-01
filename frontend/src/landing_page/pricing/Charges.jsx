01e15b28d0a985a7a592846fe744ea46b313986f
Pragyan Lahkar <pragyanlahkar28@gmail.com>
2025-11-01 13:04:33 +0530

    added new env variables

--- frontend/src/landing_page/pricing/Charges.jsx
+++ frontend/src/landing_page/pricing/Charges.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Charges.css"; // Import CSS file
+import "./charges.css"; // Import CSS file
 
 function Charges() {
     const [activeTab, setActiveTab] = useState("equities");
